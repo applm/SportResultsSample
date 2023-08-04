fb3851dd7b3ba6752bb4f0ef7d94dff344f4c148
Martin Appl <martin.appl@budgetbakers.com>
2023-08-04 23:29:34 +0200

    Raise gradle version.
    Dictated also java version raise.

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -21,8 +21,8 @@ plugins {
 group = "com.mappl.sportresultsample.buildlogic"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
